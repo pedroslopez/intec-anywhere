ae3899c09672fb1b1fdc6937f76ef22f1e84ac62
Pedro Lopez <pedroslopez@me.com>
2018-08-22 11:09:09 -0400

    [FIX] Allow origin headers (🤦🏽‍♂️)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ var bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use('/procesos', require('./router/procesos'));
